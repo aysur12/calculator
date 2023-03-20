719d8565c2db5f1e417cfbe513b70587f9dc3c6b
Aysur Kuchkashev <aysursabin@gmail.com>
2023-03-20 16:32:04 +0700

    добавил поиск html-элементов через querySelector

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const firstInput = document.querySelector('.js-calculator__input_first');
+const secondInput = document.querySelector('.js-calculator__input_second');
+const operationsSelect = document.querySelector('.js-calculator__select');
+const resultButton = document.querySelector('js-calculator__button');
+const output = document.querySelector('js-calculator__output');
+

c838d31a8f5ce2e808f85a7f0e8512b4b1e283f6
Aysur Kuchkashev <aysursabin@gmail.com>
2023-03-20 17:02:44 +0700

    удалил console.log

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const operationsSelect = document.querySelector('.js-calculator__select');
 const resultButton = document.querySelector('.js-calculator__button');
 const output = document.querySelector('.js-calculator__output');
 
-console.log(typeof operationsSelect.value);
-
 resultButton.addEventListener('click', () => {
   firstNumber = parseInt(firstInput.value, 10);
   secondNumber = parseInt(secondInput.value, 10);

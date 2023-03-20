3c80cd760e8d47369a19126dceafb5045bd61798
Aysur Kuchkashev <aysursabin@gmail.com>
2023-03-20 16:43:54 +0700

    навесил обработчик на кнопку, преобразовал значения инпутов в число

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ const secondInput = document.querySelector('.js-calculator__input_second');
 const operationsSelect = document.querySelector('.js-calculator__select');
 const resultButton = document.querySelector('.js-calculator__button');
 const output = document.querySelector('.js-calculator__output');
+
+resultButton.addEventListener('click', () => {
+  firstNumber = parseInt(firstInput.value, 10);
+  secondNumber = parseInt(secondInput.value, 10);
+});
